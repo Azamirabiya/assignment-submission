209d6fd6f74ec07748d0724224109965fd4f1b5d
Azamirabiya <77433503+Azamirabiya@users.noreply.github.com>
2021-09-17 18:07:01 +0530

    Add files via upload

--- day1.java
+++ day1.java
@@ -1,18 +1,34 @@
-/*Differences between Interpreter and Compiler*/
+class Student {
+    //data members
+    String name;
+    int roll;
+    float marks;
 
-Interpreter
+    void show() { //method
+        System.out.println("name: " name,"roll: " roll,"marks: "marks)
+    }
 
-Interpreter translates just one statement of the program at a time into machine code.
-An interpreter takes very less time to analyze the source code. However, the overall time to execute the process is much slower.
-An interpreter does not generate an intermediary code. Hence, an interpreter is highly efficient in terms of its memory.
-Keeps translating the program continuously till the first error is confronted. If any error is spotted, it stops working and hence debugging becomes easy.
-Interpreters are used by programming languages like Ruby and Python for example.
+    public static void main(String[] args) {
+        Student ob = new Student();
 
+        Student ob - Student.main(String[])
+        ob.show();
+    }
+} 
 
+class StudentDrive {
 
-Compiler
-Compiler scans the entire program and translates the whole of it into machine code at once.
-A compiler takes a lot of time to analyze the source code. However, the overall time taken to execute the process is much faster.
-A compiler always generates an intermediary object code. It will need further linking. Hence more memory is needed.
-A compiler generates the error message only after it scans the complete program and hence debugging is relatively harder while working with a compiler.
-Compliers are used by programming languages like C and C++ for example.
+    public static void main(String[] args) {
+        Student ob1 = new Student();
+        Student ob2 = new Student();
+        Student ob3 = new Student();
+
+        ob1.name = "Rabiya";
+        ob2.roll = 20;
+        ob3.marks = 88.32;
+        ob1.show();
+        ob2.show();
+        ob3.show();
+
+    }
+}
